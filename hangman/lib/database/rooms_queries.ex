99eed9aeaee56240444f2132b51c71bbda337df8
hrist-todorova <hrist_todorova@abv.bg>
2017-08-19 20:06:45 +0300

    add method which returns all available rooms

--- hangman/lib/database/rooms_queries.ex
+++ hangman/lib/database/rooms_queries.ex
@@ -1,10 +1,15 @@
 defmodule Hangman.Rooms.Queries do
 	import Ecto.Query
 
-  def add_user_to_room(room_name, user) do
-    Database.Repo.insert(%Database.Room{name: "#{room_name}", username: "#{user}", wins: 0})
+	def add_user_to_room(room_name, user) do
+		Database.Repo.insert(%Database.Room{name: "#{room_name}", username: "#{user}", wins: 0})
 	end
 	
-
+	def print_all_rooms do
+		"rooms"
+		|> select([:name])
+		|> Database.Repo.all
+		|> Enum.uniq
+	end
 
 end
