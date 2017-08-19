111a8f7acfe13c975fbf83c1b71de33bc8826c0e
hrist-todorova <hrist_todorova@abv.bg>
2017-08-19 16:06:30 +0300

    add users schema

---
+++ hangman/lib/database/users.ex
@@ -0,0 +1,10 @@
+defmodule Database.User do
+    use Ecto.Schema
+  
+    schema "users" do
+      field :username, :string
+      field :password, :string
+      field :wins, :integer
+      field :total, :integer
+    end
+  end
